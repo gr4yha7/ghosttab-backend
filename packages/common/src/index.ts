917fc9af1170c050a288db0ecd3c8bda61d0238f
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2025-12-30 23:37:19 +0100

    feat: common package exports

---
+++ packages/common/src/index.ts
@@ -0,0 +1,52 @@
+// Config exports
+export { supabase, getSupabaseClient } from './config/supabase';
+export { redis, getRedisClient, getNotificationChannel, publishNotification } from './config/redis';
+export { logger } from './config/logger';
+
+// Type exports
+export type { Database } from './types/database.types';
+export type {
+  NotificationType,
+  FriendshipStatus,
+  TabStatus,
+  TransactionType,
+} from './types/states.types';
+export type {
+  AuthenticatedRequest,
+  ApiResponse,
+  PaginationParams,
+  NotificationPayload,
+  TabCreatedPayload,
+  FriendRequestPayload,
+  PaymentSettledPayload,
+} from './types/api.types';
+
+// Error exports
+export {
+  AppError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ValidationError,
+  ConflictError,
+  InternalServerError,
+} from './utils/errors';
+
+// Utility exports
+export { sendSuccess, sendError, sendCreated, sendNoContent } from './utils/response';
+
+// Validator exports
+export {
+  emailSchema,
+  uuidSchema,
+  walletAddressSchema,
+  paginationSchema,
+  idParamSchema,
+  otpCodeSchema,
+  decimalAmountSchema,
+  txHashSchema,
+} from './validators/common.validators';
+
+// Middleware exports
+export { errorHandler, notFoundHandler } from './middleware/errorHandler';
+export { validate } from './middleware/validate';
